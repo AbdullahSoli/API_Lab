83bb93990be74d57696b038e92a0cdb869722503
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-29 06:06:27 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,9 +7,7 @@ st.title("Football Player Prediction")
 age = st.number_input("Age", min_value=0)
 appearances = st.number_input("Appearances", min_value=0)
 goals = st.number_input("Goals", min_value=0)
-
 position = st.text_input("Enter Position", "")
-
 
 # Button to submit data
 if st.button("Predict"):
@@ -19,14 +17,15 @@ if st.button("Predict"):
         "goals": goals,
         "position": position
     }
-    # Send data to FastAPI
-    response = requests.post("https://api-u65r.onrender.com/predict", json=player_data)
-    prediction = response.json()["prediction"]
+
     try:
+        # Send data to FastAPI
         response = requests.post("https://api-u65r.onrender.com/predict", json=player_data)
         response.raise_for_status()  # Will raise an HTTPError for bad responses
+
+        # Extract and display prediction
         prediction = response.json().get("prediction", "No prediction found")
         st.write(f"Prediction: {prediction}")
+        
     except requests.exceptions.RequestException as e:
         st.error(f"An error occurred: {e}")
-    st.write(f"Prediction: {prediction}")
